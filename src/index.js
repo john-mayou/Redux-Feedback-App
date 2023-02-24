c99bc4c3addd516f7b016fe74fa965b7b74c31a2
John Mayou <johnm.ceo@gmail.com>
2023-02-23 21:39:39 -0600

    created redux reducers in index.js and updated dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+
+const answers = (
+	state = {
+		feeling: 4,
+		understanding: 6,
+		support: 3,
+		comments: "This is a comment",
+	},
+	action
+) => {
+	switch (action.type) {
+		case "ADD_ANSWER":
+			return { ...state, ...action.payload };
+		case "UPDATE_ANSWER":
+			const newState = { ...state };
+			newState[action.payload.question] = action.payload.answer;
+			return newState;
+		case "CLEAR_ANSWERS":
+			return {};
+		default:
+			return state;
+	}
+};
+
+const storeInstance = createStore(
+	combineReducers({ answers }),
+	applyMiddleware(logger)
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={storeInstance}>
+			<App />
+		</Provider>
+	</React.StrictMode>
 );
