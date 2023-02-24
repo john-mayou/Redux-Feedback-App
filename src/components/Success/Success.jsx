d615e68479c06e40020acf93bf9a7a7d268f895c
John Mayou <johnm.ceo@gmail.com>
2023-02-23 22:01:02 -0600

    added feedback form reset button on success page

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,5 +1,20 @@
+import { useHistory } from "react-router-dom";
+
 function Success() {
-	return <h1>You have successfully submitted the feedback! Thank you!</h1>;
+	const history = useHistory();
+
+	const handleResetFeedbackForm = () => {
+		history.push("/");
+	};
+
+	return (
+		<>
+			<h1>You have successfully submitted the feedback! Thank you!</h1>
+			<button onClick={handleResetFeedbackForm}>
+				Leave New Feedback
+			</button>
+		</>
+	);
 }
 
 export default Success;
