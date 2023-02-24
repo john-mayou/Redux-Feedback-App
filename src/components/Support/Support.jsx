144868913ad0588109a9b1487f8302b84109ba90
John Mayou <johnm.ceo@gmail.com>
2023-02-23 21:35:10 -0600

    added Support component

---
+++ src/components/Support/Support.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+function Support() {
+	const history = useHistory();
+	const dispatch = useDispatch();
+
+	const [supportInput, setSupportInput] = useState(0);
+
+	const handleNextPage = () => {
+		if (supportInput < 1 || supportInput > 6 || !supportInput) {
+			alert("Please provide an answer between 1 and 6");
+			return;
+		}
+		dispatch({
+			type: "ADD_ANSWER",
+			payload: {
+				support: supportInput,
+			},
+		});
+		history.push("/feedback/question4");
+	};
+
+	return (
+		<section className="question__section">
+			<h1 className="question-header">
+				How well are you being supported?
+			</h1>
+			<label className="question-label">Support?</label>
+			<input
+				className="question-input"
+				value={supportInput}
+				onChange={(e) => setSupportInput(Number(e.target.value))}
+				type="number"
+			/>
+			<button className="next-button" onClick={handleNextPage}>
+				NEXT
+			</button>
+		</section>
+	);
+}
+
+export default Support;
